1b786914be28e365550c9662ee1f7c292f291b7a
DaniyalWaheed <daniyalwaheed975@gmail.com>
2020-11-19 18:22:32 +0500

    before pulling

--- Comparison/views.py
+++ Comparison/views.py
@@ -7,7 +7,7 @@ from rest_framework.parsers import FileUploadParser
 from rest_framework.views import APIView
 # import pandas as pd
 
-
+import os
 import numpy as np
 import pandas as pd
 import sklearn as sklearn
@@ -29,7 +29,7 @@ from rest_framework import response, decorators, permissions, status
 
 def readCsv():
     data = pd.read_csv(
-        'E:\\FYP\\Defect-Prediction\\projectapi\\encoded_format.csv', decimal=',')
+        os.getcwd()+'\\Comparison\\encoded_format.csv', decimal=',')
     X = data.drop(["Defect Density"], axis=1)
     y = data["Defect Density"]
     # X = X.drop(["Total Defects Delivered"], axis=1)
@@ -69,6 +69,7 @@ def conversion_to_defects(data):
 @decorators.permission_classes([permissions.AllowAny])
 def getTwoMLAlgoNames(request):
     print(request.data)
+    print("PAth.      :", os.getcwd())
     ml1 = applyMLAlgoWithoutInputValues(
         request.data["m1"],
         request.data["features"],
