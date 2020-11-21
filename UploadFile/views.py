4c88ec5bc0610a9074a8bca809e186470cabcac5
haseeb129 <haseeb129ciit@gmail.com>
2020-11-21 19:03:39 +0500

    uploaded dataset Working

--- UploadFile/views.py
+++ UploadFile/views.py
@@ -20,75 +20,94 @@ def upload(request):
         raise ParseError("Empty content")
     f = request.data['file']
     filename = request.data['name']
-    overwrite = request.data['overwrite']
-    oldname = request.data['oldname']
     pandafile = pd.read_csv(f, encoding="ISO-8859-1", error_bad_lines=False)
     pandafile = make(pandafile)
     df = pd.DataFrame(pandafile)
-    # print("df", pandafile)
-    if(overwrite == 'true'):
-        obj = get_object_or_404(csvName, name=oldname)
-        obj.delete()
-        db = csvName(name=filename)
-        db.save()
-        df.to_csv(filename, index=False)
-    else:
-        db = csvName(name=filename)
-        db.save()
-        df.to_csv(filename, index=False)
-        pand = pd.read_csv(filename, encoding="ISO-8859-1",
-                           error_bad_lines=False)
-    return Response(pandafile)
+    db = csvName(name=filename)
+    path = './csv/'+filename
+    columns=pandafile.columns
+    df.to_csv(path, index=False, header=True)
+    db.save()
+    # df.to_csv(filename, index=False)
+    # pand = pd.read_csv(path, encoding="ISO-8859-1",error_bad_lines=False)
+    return Response({"pandafile":pandafile,"columns":columns,"info": pandafile.info(verbose=False),"filePath":path})
 
 
 @decorators.api_view(["POST"])
 @decorators.permission_classes([permissions.AllowAny])
-def read(request):
-    filename = request.data['name']
-    csv = pd.read_csv(filename)
-    # print(csv)
-    return Response(csv)
+def readUploadedFile(request):
+    print("request", request.data)
+    filePath = request.data['filePath']
+    pandafile = pd.read_csv(filePath, encoding="ISO-8859-1", error_bad_lines=False)
+    pandafile = make(pandafile)   
+    columns=pandafile.columns
+    return Response({"pandafile":pandafile,"columns":columns,"info": pandafile.info(verbose=False),"filePath":filePath})
+
 
 
 @decorators.api_view(["POST"])
 @decorators.permission_classes([permissions.AllowAny])
 def columnDetails(request):
     print("columnDetails Request", request.data)
-    filename = request.data['name']
-    pandafile = pd.read_csv(filename)
+    filePath = request.data['filePath']
+    pandafile = pd.read_csv(filePath)
     column = request.data['column']
     dataColumn = pandafile[column]
-    if(dataColumn.dtypes == object):
-        # print("Dealing with object")
-        nullvales = (dataColumn == '').sum()
-        dic = {"Null Values": nullvales, "Not Null Values": dataColumn.notnull().sum(
-            axis=0)-nullvales, "type": "String/Object", "Unique Values": objetToDict(dataColumn.value_counts(dropna=False))}
-    else:
-        # print("Dealing with Number")
-        nullvales = (pandafile[column] == 123456789).sum()
-
-        dic = {"Min": dataColumn.min(),
-               "Max": dataColumn.max(),
-               "Mean": dataColumn.mean(axis=0),
-               "Mediam": dataColumn.median(axis=0),
-               "Standerd Deviation": dataColumn.std(axis=0),
-               "Null Values": nullvales,
-               "Not Null Values": dataColumn.notnull().sum(axis=0)-nullvales, "type": "INT-FLOAT"}
-    # print("column,", dic)
+    dic=dataColumnDetails(dataColumn)
+    # if(dataColumn.dtypes == object):
+    #     # print("Dealing with object")
+    #     # nullvales = (dataColumn == '').sum()
+    #     dic =objectColumnsDetails(dataColumn) 
+        
+    #     # {"Null Values": nullvales, "Not Null Values": dataColumn.notnull().sum(
+    #     #     axis=0)-nullvales, "type": "String/Object", "Unique Values": objetToDict(dataColumn.value_counts(dropna=False))}
+    # else:
+    #     dic =numaricColumnsDetails(dataColumn)
+    #     # print("Dealing with Number")
+    # # print("column,", dic)
 
     return Response(dic)
+
+
+def dataColumnDetails(dataColumn):
+    if(dataColumn.dtypes == object):
+        nullvales = (dataColumn == 'Null').sum()
+        return {"Null Values": nullvales, "Not Null Values": dataColumn.notnull().sum(axis=0)-nullvales, "type": "String/Object", "Unique Values": objetToDict(dataColumn.value_counts(dropna=False))}
+    else:
+        nullvales = (dataColumn== 123456789).sum()
+        return {"Min": dataColumn.min(),
+                    "Max": dataColumn.max(),
+                    "Mean": dataColumn.mean(axis=0),
+                    "Mediam": dataColumn.median(axis=0),
+                    "Standerd Deviation": dataColumn.std(axis=0),
+                    "Null Values": nullvales,
+                    "Not Null Values": dataColumn.notnull().sum(axis=0)-nullvales, "type": "INT-FLOAT"}
+ 
+        
+
+
+# def numaricColumnsDetails(dataColumn):
+#     nullvales = (dataColumn== 123456789).sum()
+#     return {"Min": dataColumn.min(),
+#                 "Max": dataColumn.max(),
+#                 "Mean": dataColumn.mean(axis=0),
+#                 "Mediam": dataColumn.median(axis=0),
+#                 "Standerd Deviation": dataColumn.std(axis=0),
+#                 "Null Values": nullvales,
+#                 "Not Null Values": dataColumn.notnull().sum(axis=0)-nullvales, "type": "INT-FLOAT"}
+ 
 
 
 @decorators.api_view(["POST"])
 @decorators.permission_classes([permissions.AllowAny])
 def deleteColumn(request):
     print("deleteColumn Request", request.data)
-    filename = request.data['name']
-    pandafile = pd.read_csv(filename)
+    filePath = request.data['filePath']
+    pandafile = pd.read_csv(filePath)
     column = request.data['column']
     dataColumn = pandafile[column]
     pandafile = pandafile.drop([request.data['column']], axis='columns')
-    savePandaFile1(pandafile, filename)
+    savePandaFile1(pandafile, filePath)
     return Response({"pandafile": pandafile, "columns": pandafile.columns, })
 
 
@@ -96,8 +115,8 @@ def deleteColumn(request):
 @decorators.permission_classes([permissions.AllowAny])
 def fillNullValuesOfColumn(request):
     print("fillNullValuesOfColumn Request", request.data)
-    filename = request.data['name']
-    pandafile = pd.read_csv(filename)
+    filePath = request.data['filePath']
+    pandafile = pd.read_csv(filePath)
     pandafile = make(pandafile)
     column = pandafile[request.data['column']]
     method = request.data['method']
@@ -108,36 +127,36 @@ def fillNullValuesOfColumn(request):
     elif (method == "REPLACE"):
         stringValue = request.data['replaceValue']
     pandafile[request.data['column']].replace(
-        123456789, stringValue, inplace=True)
-    savePandaFile1(pandafile, filename)
-    return Response({"pandafile": pandafile, "columns": pandafile.columns, })
+        123456789, float(stringValue), inplace=True)
+    savePandaFile1(pandafile, filePath)
+    return Response({"pandafile": pandafile, "columns": pandafile.columns,"ColumnDetails":dataColumnDetails(column) })
 
 
 @decorators.api_view(["POST"])
 @decorators.permission_classes([permissions.AllowAny])
 def fillNullValuesOfColumnString(request):
     print("fillNullValuesOfColumnString Request", request.data)
-    filename = request.data['name']
-    pandafile = pd.read_csv(filename)
+    filePath = request.data['filePath']
+    pandafile = pd.read_csv(filePath)
     pandafile = make(pandafile)
     column = pandafile[request.data['column']]
     method = request.data['method']
     if (method == "REPLACE"):
         dataColumn = pandafile[request.data['column']
-                               ].replace('', request.data['replaceValue'], inplace=True)
+                               ].replace("Null", request.data['replaceValue'], inplace=True)
     elif (method == "REPLACEVALUE"):
 
         pandafile[request.data['column']].replace(
             request.data['to_replace_value'], request.data['to_value'], inplace=True)
-    savePandaFile(pandafile, filename)
-    return Response({"pandafile": pandafile, "columns": pandafile.columns, })
+    savePandaFile1(pandafile, filePath)
+    return Response({"pandafile": pandafile, "columns": pandafile.columns,"ColumnDetails":dataColumnDetails(column) })
 
 
 def make(pandafile):
     for i in pandafile.columns:
         if(pandafile[i].dtypes == object):
             # print("Dealing with object")
-            pandafile[i] = pandafile[i].fillna("")
+            pandafile[i] = pandafile[i].fillna("Null")
         else:
             # print("Dealing with Number")
             pandafile[i] = pandafile[i].fillna(123456789)
@@ -170,6 +189,4 @@ def savePandaFile(pandafile, fileName):
 
 def savePandaFile1(pandafile, fileName):
     df = pd.DataFrame(pandafile)
-    # db = csvName(name=fileName)
-    # db.save()
     df.to_csv(fileName, index=False)
